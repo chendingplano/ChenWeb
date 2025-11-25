c6bacf1dde156472bd7cc4d2e83a1b64f86a996c
Chen Ding <chending0602@hotmail.com>
2025-11-25 05:16:05 -0600

    Added svelte-form-components

---
+++ web/svelte-form-components/src/lib/index.ts
@@ -0,0 +1,3 @@
+export { default as FormItemSelect } from './lib/form/FormItemSelect.svelte';
+export { default as FormItemInput } from './form/FormItemInput.svelte';
+// Export other components as needed
