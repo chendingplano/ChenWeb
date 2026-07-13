e501958f67742c293a7d45ef4aad2c30fd7c765a
Chen Ding <chending0602@hotmail.com>
2026-07-13 12:04:42 -0500

    test: cover owner validation in sql store

--- server/api/doc-benchmark/store_sqlmock_test.go
+++ server/api/doc-benchmark/store_sqlmock_test.go
@@ -2,6 +2,7 @@ package docbenchmark
 
 import (
 	"context"
+	"database/sql"
 	"database/sql/driver"
 	"github.com/DATA-DOG/go-sqlmock"
 	"testing"
@@ -18,6 +19,16 @@ func TestSQLStoreCreateCaseRunCanonicalJSON(t *testing.T) {
 	if e := m.ExpectationsWereMet(); e != nil {
 		t.Fatal(e)
 	}
+}
+func TestSQLStoreOwnerXOR(t *testing.T) {
+	db, m, _ := sqlmock.New()
+	defer db.Close()
+	s := SQLStore{DB: db}
+	_, e := s.InsertScore(context.Background(), ScoreRecord{AttemptID: sql.NullString{String: "a", Valid: true}, RunID: sql.NullString{String: "r", Valid: true}})
+	if e == nil {
+		t.Fatal("expected owner xor error")
+	}
+	_ = m
 }
 
 var _ driver.Value
